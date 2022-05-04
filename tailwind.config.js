87a4c79e4ce85e21989558a8ee7faa02452affb0
Ravi Kiran Pilli <ravi.pilli@ispace.com>
2022-05-04 18:07:11 +0530

    Added project using nextjs and tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    // colors: {
+    //   "#0f172a"
+    //   // Configure your color palette here
+    // },
+    extend: {},
+  },
+  plugins: [],
+};
